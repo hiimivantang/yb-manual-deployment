6f05334a93b8b520b41e9b3660f2df7f73129809
hiimivantang <hiimivantang@gmail.com>
2023-03-12 22:53:05 +0800

    updated shell scripts to accept same arguments

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-MASTER_ADDRESSES="$1"
+NODE1_IP="$1"
+shift 1
+NODE2_IP="$1"
+shift 1
+NODE3_IP="$1"
+shift 1
 
-if [ -z "$MASTER_ADDRESSES" ]; then
-	echo "Usage $0 <master addresses and their respective ports delimited by comma. E.g 192.168.0.22:7100,192.168.0.23:7100,192.168.0.24:7100>"
-  echo ""
-  exit 1
+if [[ -z "$NODE1_IP" ]] | [[ -z "$NODE2_IP" ]] | [[ -z "$NODE3_IP" ]]; then
+    echo "Usage $0 <node 1 ip address> <node 2 ip address> <node 3 ip address>"
+      exit 1
 fi
 
 sudo apt update -y
@@ -28,7 +32,7 @@ sudo chown ubuntu:ubuntu /etc/yugabyte/tserver/conf/server.conf
 PRIVATE_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 tee /etc/yugabyte/master/conf/server.conf <<EOF
---master_addresses=$MASTER_ADDRESSES
+--master_addresses=$NODE1_IP:7100,$NODE2_IP:7100,$NODE3_IP:7100
 --rpc_bind_addresses=$PRIVATE_IP
 --fs_data_dirs=/home/ubuntu/data
 --placement_cloud=aws
@@ -68,7 +72,7 @@ sudo systemctl start yb-master
 
 
 tee /etc/yugabyte/tserver/conf/server.conf <<EOF
---tserver_master_addrs=$MASTER_ADDRESSES
+--tserver_master_addrs=$NODE1_IP:7100,$NODE2_IP:7100,$NODE3_IP:7100
 --rpc_bind_addresses=$PRIVATE_IP:9100
 --enable_ysql
 --pgsql_proxy_bind_address=$PRIVATE_IP:5433
--- start_prometheus.sh
+++ start_prometheus.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+NODE1_IP="$1"
+shift 1
+NODE2_IP="$1"
+shift 1
+NODE3_IP="$1"
+shift 1
+
+if [[ -z "$NODE1_IP" ]] | [[ -z "$NODE2_IP" ]] | [[ -z "$NODE3_IP" ]]; then
+  echo "Usage $0 <node 1 ip address> <node 2 ip address> <node 3 ip address>"
+  exit 1
+fi
+
 wget https://github.com/prometheus/prometheus/releases/download/v2.42.0/prometheus-2.42.0.linux-amd64.tar.gz
 
 tar xvfz prometheus-2.42.0.linux-amd64.tar.gz && cd prometheus-2.42.0.linux-amd64
@@ -44,26 +56,25 @@ scrape_configs:
         replacement: "rpc_latency$4"
 
     static_configs:
-      - targets: ["$PRIVATE_IP:7000"]
+      - targets: ["$NODE1_IP:7000", "$NODE2_IP:7000", "$NODE3_IP:7000"]
         labels:
           export_type: "master_export"
 
-      - targets: ["$PRIVATE_IP:9000"]
+      - targets: ["$NODE1_IP:9000", "$NODE2_IP:9000", "$NODE3_IP:9000"]
         labels:
           export_type: "tserver_export"
 
-      - targets: ["$PRIVATE_IP:12000"]
+      - targets: ["$NODE1_IP:12000", "$NODE2_IP:12000", "$NODE3_IP:12000"]
         labels:
           export_type: "cql_export"
 
-      - targets: ["$PRIVATE_IP:13000"]
+      - targets: ["$NODE1_IP:13000", "$NODE2_IP:13000", "$NODE3_IP:13000"]
         labels:
           export_type: "ysql_export"
 
-      - targets: ["$PRIVATE_IP:11000"]
+      - targets: ["$NODE1_IP:11000", "$NODE2_IP:11000", "$NODE3_IP:11000"]
         labels:
           export_type: "redis_export"
-
 EOF
 
 
